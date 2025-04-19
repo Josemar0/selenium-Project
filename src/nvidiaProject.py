ac77d09cd22c3d4b552ed33a473a20c804f8b7df
josemar0 <josemar.ochoa80@bcmail.cuny.edu>
2025-04-19 19:59:48 -0400

    edited nvidiaProject

--- src/nvidiaProject.py
+++ src/nvidiaProject.py
@@ -1,4 +1,5 @@
 from selenium import webdriver;
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,35 +7,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
+import os
 
-def wait_and_click(element, wait_time=0) :
-    time.sleep(wait_time)
-    element.click
+#describes session,require for remote
+option = webdriver.ChromeOptions()
+option.timeouts = {'implicit': 5}
+option.binary_location = "/usr/bin/chromium-browser"
 
-driver = webdriver.Chrome()
+#creates path to chromedriver
+path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(path,"..","lib","chromedriver-linux64","chromedriver")
 
+#manages starting and stopping of local drivers, not remote
+services = Service(executable_path=path)
+
+#what controls/launches the browser
+driver = webdriver.Chrome(options=option, service=services)
 driver.get("https://www.nvidia.com/en-us/geforce/graphics-cards/")
 
 WebDriverWait(driver,5).until(
     EC.presence_of_element_located((By.CLASS_NAME, "sub-brand-item"))
  )
 product_menu = driver.find_element(By.CLASS_NAME, "sub-brand-item")
-
-wait_and_click(product_menu)
+product_menu.click()
 
 WebDriverWait(driver,5).until(
     EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Graphics Cards & Desktops')]"))
 )
 product_type = driver.find_element(By.XPATH, "//div[contains(text(),'Graphics Cards & Desktops')]")
 
-wait_and_click(product_type)
+product_type.click()
 
 WebDriverWait(driver,5).until(
     EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'RTX 5090')]"))
 )
 product_name = driver.find_element(By.XPATH, "//div[contains(text(),'RTX 5090')]")
 
-wait_and_click(product_name)
 
 time.sleep(5)
 
