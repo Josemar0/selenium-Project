9218464cc840495d5aebd78f40711e151cdd868d
josemar0 <josemar.ochoa80@bcmail.cuny.edu>
2025-04-19 21:56:18 -0400

    started code over

--- src/nvidiaProject.py
+++ src/nvidiaProject.py
@@ -1,50 +1,38 @@
 from selenium import webdriver;
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
 import os
+import re
 
-#describes session,require for remote
-option = webdriver.ChromeOptions()
-option.timeouts = {'implicit': 5}
-option.binary_location = "/usr/bin/chromium-browser"
-
-#creates path to chromedriver
-path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(path,"..","lib","chromedriver-linux64","chromedriver")
-
-#manages starting and stopping of local drivers, not remote
-services = Service(executable_path=path)
-
-#what controls/launches the browser
-driver = webdriver.Chrome(options=option, service=services)
+driver = webdriver.Chrome()
 driver.get("https://www.nvidia.com/en-us/geforce/graphics-cards/")
 
 WebDriverWait(driver,5).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "sub-brand-item"))
+    EC.presence_of_element_located((By.CLASS_NAME, "cmp-teaser__title"))
  )
-product_menu = driver.find_element(By.CLASS_NAME, "sub-brand-item")
-product_menu.click()
+series_options = driver.find_elements(By.CLASS_NAME, "cmp-teaser__title")
 
-WebDriverWait(driver,5).until(
-    EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Graphics Cards & Desktops')]"))
-)
-product_type = driver.find_element(By.XPATH, "//div[contains(text(),'Graphics Cards & Desktops')]")
+for series in series_options:
+    print(series.text)
 
-product_type.click()
+buttons = driver.find_elements(By.TAG_NAME,"a")
 
-WebDriverWait(driver,5).until(
-    EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'RTX 5090')]"))
-)
-product_name = driver.find_element(By.XPATH, "//div[contains(text(),'RTX 5090')]")
+series = input("Which Graphic Card Series? ")
 
+print("searching...")
+for button in buttons:
+    link = button.get_attribute("href")
+    if link and series in link:
+        WebDriverWait(driver,5).until(
+            EC.element_to_be_clickable(button)
+        )
+        print("clicking...")
+        button.click()
+        break
 
 time.sleep(5)
-
 driver.quit()
 
